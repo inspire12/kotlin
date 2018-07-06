dd017897156cf9dd94e6ec8df212e54b4785eea0
Georgy Bronnikov <georgy.bronnikov@jetbrains.com>
2018-07-06 18:20:26 +0300

    Make @jvmStatic work for companion objects on JVM_IR

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/IrDeclarationOrigin.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/IrDeclarationOrigin.kt
@@ -37,6 +37,7 @@ interface IrDeclarationOrigin {
     object IR_EXTERNAL_DECLARATION_STUB : IrDeclarationOriginImpl("IR_EXTERNAL_DECLARATION_STUB")
     object IR_BUILTINS_STUB : IrDeclarationOriginImpl("IR_BUILTINS_STUB")
     object BRIDGE : IrDeclarationOriginImpl("BRIDGE")
+    object JVM_STATIC_WRAPPER : IrDeclarationOriginImpl("JVM_STATIC_WRAPPER")
 }
 
 abstract class IrDeclarationOriginImpl(val name: String) : IrDeclarationOrigin {
